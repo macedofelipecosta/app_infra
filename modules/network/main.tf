459f235a1b30519193f61b54228b5e22b9121db9
Felipe Macedo Costa <128637344+macedofelipecosta@users.noreply.github.com>
2025-06-14 23:56:33 -0300

    Se corrige el problema de la asignación de las subredes a la tabla de ruteo privada

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -91,3 +91,8 @@ resource "aws_route" "private_nat_access" {
   nat_gateway_id = aws_nat_gateway.nat.id
 }
 
+resource "aws_route_table_association" "private" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
